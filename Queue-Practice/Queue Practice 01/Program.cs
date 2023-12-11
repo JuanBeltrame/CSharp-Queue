f344f37e58270b5064de1518d13071a89261abe6
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-11 00:28:00 -0300

    Update

--- Queue-Practice/Queue Practice 01/Program.cs
+++ Queue-Practice/Queue Practice 01/Program.cs
@@ -1,4 +1,5 @@
-﻿// Las Colecciones en C# son objetos, por lo tanto ademas, son tipos de referencia
+﻿using Queue_Practice_01;
+// Las Colecciones en C# son objetos, por lo tanto ademas, son tipos de referencia
 
 // Forma 1: Declaracion y asignacion en una Linea sin valores iniciales
 Queue<int> cola01 = new Queue<int>();
@@ -7,8 +8,23 @@ Queue<int> cola01 = new Queue<int>();
 Queue<int> cola02 = new Queue<int>(10);
 
 // Forma 3: Declaración e inicialización en una sola línea con elementos
-Queue<int> cola03 = new Queue<int> { 1, 2, 3, 4, 5 };
+// Esta forma no se puede, o bien, desconozco como hacerlo
 
 // Forma 4: Declaracion y asignacion en varias Lineas
 Queue<int> cola04;
 cola04 = new Queue<int>();
+
+//---------------------------
+// Forma 1: Declaracion y asignacion en una Linea sin valores iniciales
+Queue<Auto> autoCola01 = new Queue<Auto>();
+
+// Forma 2: Declaración y asignación en una línea sin valores iniciales con capacidad inicial especificada
+Queue<Auto> autoCola02 = new Queue<Auto>(10);
+
+// Forma 3: Declaración e inicialización en una sola línea con elementos
+// Esta forma no se puede, o bien, desconozco como hacerlo
+
+// Forma 4: Declaracion y asignacion en varias Lineas
+Queue<Auto> autoCola04;
+autoCola04 = new Queue<Auto>();
+
---
+++ Queue-Practice/Queue Practice 02/Program.cs
@@ -0,0 +1,30 @@
+﻿//-----------Programa Principal----------------
+
+Queue<string> clientes = new Queue<string>();
+
+clientes.Enqueue("Mauricio");
+clientes.Enqueue("Lucas");
+clientes.Enqueue("Laurato");
+
+Console.WriteLine("--------------");
+// Me devuelve el primer elemento que esta en la cola pero sin eliminarlo
+Console.Write("Muetro el primero elemento de la cola: ");
+Console.WriteLine(clientes.Peek());
+
+Console.WriteLine("--------------");
+foreach (string item in clientes)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("--------------");
+// Me devuelve el primer elemento que esta en la cola y lo elimina
+Console.WriteLine(clientes.Dequeue());
+
+Console.WriteLine("--------------");
+foreach (string item in clientes)
+{
+    Console.WriteLine(item);
+}
+
+
