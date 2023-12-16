55249e9da467b8b4fbca6e937090a9fade9f40e3
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-16 13:04:54 -0300

    Update

--- Queue-Practice/Queue Practice 01/Program.cs
+++ Queue-Practice/Queue Practice 01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Queue_Practice_01;
+using System.Drawing;
 // Las Colecciones en C# son objetos, por lo tanto ademas, son tipos de referencia
 
 // Forma 1: Declaracion y asignacion en una Linea sin valores iniciales
@@ -15,16 +16,21 @@ Queue<int> cola04;
 cola04 = new Queue<int>();
 
 //---------------------------
-// Forma 1: Declaracion y asignacion en una Linea sin valores iniciales
+// Forma 1: Declarar Objetos // Declaracion y asignacion en una Linea sin valores iniciales
 Queue<Auto> autoCola01 = new Queue<Auto>();
 
-// Forma 2: Declaración y asignación en una línea sin valores iniciales con capacidad inicial especificada
+// Forma 2: Declarar Objetos // Declaración y asignación en una línea sin valores iniciales con capacidad inicial especificada
 Queue<Auto> autoCola02 = new Queue<Auto>(10);
 
-// Forma 3: Declaración e inicialización en una sola línea con elementos
-// Esta forma no se puede, o bien, desconozco como hacerlo
+// Forma 3: Declarar Objetos // Declaración e inicialización en una sola línea con elementos
+Queue<Auto> autoPila03 = new Queue<Auto>(new Auto[]
+{
+   new Auto(1, 12343, Color.AliceBlue, 1000),
+   new Auto(1, 12343, Color.AliceBlue, 1000),
+   new Auto(1, 12343, Color.AliceBlue, 1000)
+});
 
-// Forma 4: Declaracion y asignacion en varias Lineas
+// Forma 4: Declarar Objetos // Declaracion y asignacion en varias Lineas
 Queue<Auto> autoCola04;
 autoCola04 = new Queue<Auto>();
 
