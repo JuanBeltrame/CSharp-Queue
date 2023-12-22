302018403ef87f28dd177c2ffa9005a492b705fc
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-22 16:27:59 -0300

    Update

---
+++ Queue-Practice/Queue Practice 04 (Library)/Queue.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+
+namespace Queue_Practice_04__Library_
+{
+    public class Queue
+    {
+        public int  MaxSize { get; set; } // Sets the number of elements because this is an array
+        public int[] QueueArray { get; set; } // Actual array we will store elements in
+        public int Front { get; set; } // Index to keep track of front
+        public int Rear { get; set; } // Index to keep track of adds
+        public int NItems { get; set; } // This will keep track of lenght
+
+
+        public Queue(int size)
+        {
+            MaxSize = size;
+            QueueArray = new int[size];
+            Front = default;
+            Rear = -1;
+        }
+
+        // Somebody is going into the line
+        public void Enqueue(int item)
+        {
+            // Incremento our pointer
+            Rear++;
+            // Insert into where the rear was incremented
+            QueueArray[Rear] = item;
+            // Increment
+            NItems++;
+        }
+
+        public int Dequeue()
+        {
+            int temp = QueueArray[Front];
+            Front++;
+            if(Front == MaxSize)
+            {
+                Front = 0;
+            }
+            NItems--;
+            return temp;
+        }
+
+        public int Peek()
+        {
+            return QueueArray[Front];
+        }
+    }
+}
---
+++ Queue-Practice/Queue Practice 04 (View)/Program.cs
@@ -0,0 +1,13 @@
+﻿//-----Programa Principal----
+using Queue_Practice_04__Library_;
+
+Queue queue = new Queue(10);
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+
+queue.Dequeue();
+queue.Dequeue();
+
+queue.Peek();
